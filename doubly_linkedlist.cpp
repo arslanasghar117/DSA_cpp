59cdf4199a08b0b8100dc2593c24caf813881395
Arslan Asghar <105450223+duke-fuke@users.noreply.github.com>
2022-11-21 16:36:38 +0500

    doubly_linkedlist

---
+++ doubly_linkedlist.cpp
@@ -0,0 +1,154 @@
+#include <iostream>
+using namespace std;
+
+struct node
+{
+    int data;
+    node *next;
+    node *prev;
+};
+
+class doubly_linkedlist
+{
+    public:
+    node *first ,*last;
+
+    doubly_linkedlist()
+    {
+        first=NULL;
+        last=NULL;
+    }
+
+    void insert_first(int a)
+    {
+        if(first==NULL)
+        {
+            first= new node;
+            first->data=a;
+            first->next=NULL;
+            first->prev=NULL;
+            last=first;
+            return;
+        }
+        else
+        {
+            node *ptr=new node;
+            ptr->data=a;
+            ptr->next=first;
+            first->prev=ptr;
+            ptr->prev=NULL;
+            first=ptr;
+        }
+    }
+
+    void insert_last(int a)
+    {
+        if(first==NULL)
+        {
+            first= new node;
+            first->data=a;
+            first->next=NULL;
+            first->prev=NULL;
+            last=first;
+            return;
+        }
+        else
+        {
+            node *ptr=new node;
+            ptr->data=a;
+            ptr->next=NULL;
+            ptr->prev=last;
+            last->next=ptr;
+            last=ptr;
+        }
+    }
+
+    void remove_first()
+    {
+        if(first==NULL)
+        {
+            cout<<"empty";
+            return;
+        }
+        if(first->next==NULL)
+        {
+            delete first;
+            last=NULL;
+            return;
+        }
+        else
+        {
+            node *ptr=first;
+            first=first->next;
+            first->prev=NULL;
+            delete ptr;
+        }
+    }
+
+    void remove_last()
+    {
+        if(first==NULL)
+        {
+            cout<<"empty";
+            return;
+        }
+        if(last->prev==NULL)
+        {
+            delete last;
+            first=NULL;
+        }
+        else
+        {
+            node *ptr=last;
+            last=last->prev;
+            last->next=NULL;
+            delete ptr;
+        }
+    }
+
+    void show_forward()
+    {
+        if(first==NULL)
+        {
+            cout<<"empty";
+        }
+        else{
+            node *ptr=first;
+            while(ptr->next!=NULL){
+                cout<<ptr->data;
+                ptr=ptr->next;
+            }
+            cout<<ptr->data;
+        }
+    }
+
+    void show_backward()
+    {
+        if(first==NULL)
+        {
+            cout<<"empty";
+        }
+        else{
+            node *ptr=last;
+            while(ptr->prev!=NULL){
+                cout<<ptr->data;
+                ptr=ptr->prev;
+            }
+            cout<<ptr->data;
+        }
+    }
+};
+
+int main()
+{
+    doubly_linkedlist d;
+    d.insert_first(9);
+    d.insert_first(8);
+    d.insert_first(7);
+    d.show_forward();
+    d.show_backward();
+    d.remove_first();
+    d.show_forward();
+    d.remove_last();
+    d.show_forward();
+}
